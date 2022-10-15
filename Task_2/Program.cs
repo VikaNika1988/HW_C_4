f77aab57b3ea1ea8ee5461f7523a1ccb448baddf
viktoriya <malec1988@icloud.com>
2022-10-15 18:07:33 +0300

    принимает на вход число и выдаёт сумму цифр в числе

---
+++ Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в числе {number} = {sumofdigits(number)}");
+
+int sumofdigits(int num){
+    int sum = 0;
+    for (int i = 0; num > 0; i++)
+    {
+        i = num % 10;
+        sum += i;
+        num /= 10;      
+    }
+    return sum;
+}
